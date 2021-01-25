5b9b1aeb0d62d9637d7eb3d5eba7ed76f9067f88
CuzImClicks <bruno778.whiteelfie@gmail.com>
2021-01-25 15:50:39 +0100

    more imports

--- cogs/example_cog.py
+++ cogs/example_cog.py
@@ -3,6 +3,8 @@ import discord
 from discord.ext import commands
 from util.logger import path
 import logging
+from util import config
+import datetime
 
 lg = logging.getLogger(__name__)
 fl = logging.FileHandler(f"{path}\logs\log.log")
