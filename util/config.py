f12a05e3b69cbc9dcc56cd511f443ab0d35045a3
CuzImClicks <bruno778.whiteelfie@gmail.com>
2021-01-27 21:54:32 +0100

    fixed elif state ments
    as it seems i didn't know how they work
    added green as colour

--- util/config.py
+++ util/config.py
@@ -5,93 +5,87 @@ import discord
 
 path = str(os.getcwd())
 
-
 jf = json_util.JsonFile("config.json", f"{path}/util")
 
 
 def getLoggingLevel():
-
     level = jf.read()["level"]
 
     if level.upper() == "INFO":
 
         return logging.INFO
 
-    elif "WARNING":
+    elif level.upper() == "WARNING":
 
         return logging.WARNING
 
-    elif "DEBUG":
+    elif level.upper() == "DEBUG":
 
         return logging.DEBUG
 
-    elif "ERROR":
+    elif level.upper() == "ERROR":
 
         return logging.ERROR
 
 
 def getFileLoggingLevel():
-
     flevel = jf.read()["file_level"]
 
     if flevel.upper() == "INFO":
 
         return logging.INFO
 
-    elif "WARNING":
+    elif flevel.upper() == "WARNING":
 
         return logging.WARNING
 
-    elif "DEBUG":
+    elif flevel.upper() == "DEBUG":
 
         return logging.DEBUG
 
-    elif "ERROR":
+    elif flevel.upper() == "ERROR":
 
         return logging.ERROR
 
 
 def getToken():
-
     return jf.read()["token"]
 
 
 def getCommandPrefix():
-
     return jf.read()["command_prefix"]
 
 
 def getStatus():
-
     return jf.read()["status"]
 
 
 def getBotAccessRole():
-
     return jf.read()["Bot Access Role"]
 
 
 def getBotAdminRole():
-
     return jf.read()["Bot Admin Role"]
 
 
 def getDefaultRole():
-
     return jf.read()["Default Role"]
 
-def getKey():
 
+def getKey():
     return jf.read()["key"]
 
-def getSteamKey():
 
+def getSteamKey():
     return jf.read()["steam_key"]
 
-def getDiscordColour(colourname):
 
+def getDiscordColour(colourname):
     if colourname == "red":
         return discord.Colour(0x9D1309)
 
     elif colourname == "blue":
         return discord.Colour(0x000030)
+
+    elif colourname == "green":
+        return discord.Colour(0x0BAF07)
