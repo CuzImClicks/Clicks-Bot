443412e2d003948d4033c7481599430517be8166
CuzImClicks <bruno778.whiteelfie@gmail.com>
2021-01-27 19:54:28 +0100

    steam user

---
+++ util/steam/user.py
@@ -0,0 +1,56 @@
+import urllib.request
+import json
+from clicks_util.json_util import JsonFile
+#from util import config  # module not found error, don't know why
+import logging
+from clicks_util import logger
+
+lg = logging.getLogger(__name__[10:])
+
+
+class User:
+
+    def __init__(self, steam_id: int):
+        self.steam_id = steam_id
+
+    @property
+    def url(self):
+        return "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key="\
+               + "292177DC524546D1D4B31BA0CF3D1FF0"\
+               + "&steamids=" + str(self.steam_id) + "&format=json"
+
+    @property
+    def Request(self) -> urllib.request.Request:
+        return urllib.request.Request(self.url, data=None, headers=self.req_headers, origin_req_host=None)
+
+    @property
+    def response(self) -> urllib.request:
+        lg.info(f"Requesting player summary for user: {self.steam_id}")
+        return urllib.request.urlopen(self.Request)
+
+    @property
+    def content(self) -> str:
+        return self.response.read()
+
+    @property
+    def data(self) -> json:
+        return json.loads(self.content)
+
+    @property
+    def req_headers(self) -> dict:
+        return {"User-Agent": "Python script"}
+
+    @property
+    def player(self) -> str:
+        return self.data["response"]["players"][0]
+
+    @property
+    def name(self) -> str:
+        return self.player["personaname"]
+
+    @property
+    def state(self) -> int:
+        return int(self.player["personastate"])
+
+    def save(self):
+        jf = JsonFile(name="")
