f01ec7c5a2bf77783690fed9e522bda4d9827067
CuzImClicks <bruno778.whiteelfie@gmail.com>
2021-01-28 16:32:41 +0100

    added Clicks-BotAPI-Handler

---
+++ cogs/Clicks-BotAPI-Handler.py
@@ -0,0 +1,38 @@
+import logging
+import discord
+from discord.ext import commands
+from util.logger import path
+import logging
+from util import config
+import datetime
+from web_server.database import create_key
+
+lg = logging.getLogger(__name__[5:])
+
+
+class Clicks_BotAPI_Handler(commands.Cog):
+
+    def __init__(self, bot):
+
+        self.bot = bot
+
+    @commands.command(name="create_api_key")
+    @commands.has_role(config.getBotAdminRole())
+    async def create_api_key(self, ctx):
+        user = ctx.message.mentions[0]
+        username = user.name
+
+        key = create_key(username)
+        infoEmbed = discord.Embed(title="API Key",
+                                  description=f"A API key has been generated and saved to the database",
+                                  colour=config.getDiscordColour("green"))
+        infoEmbed.add_field(name="Key", value=key, inline=False)
+        infoEmbed.add_field(name="Requested by", value=ctx.author.name)
+        infoEmbed.set_author(name=username, icon_url=user.avatar_url)
+
+        await ctx.send(embed=infoEmbed)
+
+
+def setup(bot):
+
+    bot.add_cog(Clicks_BotAPI_Handler(bot))
