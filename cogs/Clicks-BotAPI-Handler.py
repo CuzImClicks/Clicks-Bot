005da844a54307426e4919929cda7600388f4d5f
CuzImClicks <bruno778.whiteelfie@gmail.com>
2021-01-29 21:41:25 +0100

    added get_api_key command

--- cogs/Clicks-BotAPI-Handler.py
+++ cogs/Clicks-BotAPI-Handler.py
@@ -5,7 +5,7 @@ from util.logger import path
 import logging
 from util import config
 import datetime
-from web_server.database import create_key
+from web_server.database import create_key, get_key
 
 lg = logging.getLogger(__name__[5:])
 
@@ -31,6 +31,22 @@ class Clicks_BotAPI_Handler(commands.Cog):
         infoEmbed.set_author(name=username, icon_url=user.avatar_url)
 
         await ctx.send(embed=infoEmbed)
+
+    @commands.command(name="get_api_key")
+    @commands.has_role(config.getBotAdminRole())
+    async def get_api_key(self, ctx):
+        user = ctx.message.mentions[0]
+        username = user.name
+        key = get_key(username)
+        infoEmbed = discord.Embed(title="API Key",
+                                  description=f"Read the API key for {username} from the database",
+                                  colour=config.getDiscordColour("green"))
+        infoEmbed.add_field(name="Key", value=key, inline=False)
+        infoEmbed.add_field(name="Requested by", value=ctx.author.name)
+        infoEmbed.set_author(name=username, icon_url=user.avatar_url)
+
+        await ctx.send(embed=infoEmbed)
+
 
 
 def setup(bot):
