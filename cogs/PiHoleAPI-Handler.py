79acf958c3318bf320c50d51bdf61d89220f9c6d
CuzImClicks <bruno778.whiteelfie@gmail.com>
2021-01-30 20:44:55 +0100

    pihole api handler

---
+++ cogs/PiHoleAPI-Handler.py
@@ -0,0 +1,40 @@
+import logging
+from attr import __title__
+import discord
+from discord.ext import commands
+from util.logger import path
+import logging
+from util import config
+import datetime
+import aiohttp
+from aiohttp import ClientSession
+import json
+
+
+lg = logging.getLogger(__name__[5:])
+
+
+class PiHoleAPI_Handler(commands.Cog):
+
+    def __init__(self, bot):
+
+        self.bot = bot
+
+    @commands.command(name="get_pihole_summary")
+    @commands.has_role(config.getBotAdminRole())
+    async def get_pihole_summary(self, ctx):
+        async with ClientSession() as session:
+            async with session.get(url=f"http://{config.getPiHoleIp()}/admin/api.php?summary") as data:
+                content = json.loads(await data.text())
+                infoEmbed = discord.Embed(title="Pi Hole Summary", colour=config.getDiscordColour("blue"))
+                infoEmbed.add_field(name="Blocked domains", value=content["domains_being_blocked"], inline=False)
+                infoEmbed.add_field(name="DNS queries today", value=content["dns_queries_today"], inline=False)
+                infoEmbed.add_field(name="Blocked ads today", value=content["ads_blocked_today"], inline=False)
+                infoEmbed.add_field(name="Ads blocked today", value=content["ads_blocked_today"], inline=False)
+                await ctx.send(embed=infoEmbed)
+
+        
+
+def setup(bot):
+
+    bot.add_cog(PiHoleAPI_Handler(bot))
---
+++ util/pihole/piholeapi.py
@@ -0,0 +1,123 @@
+import json
+from util import config
+import aiohttp
+from aiohttp import ClientSession
+
+class PiHoleAPI:
+
+    def __init__(self) -> None:
+        self.url = f"http://{config.getPiHoleIp()}/admin/api.php?"
+
+    @property
+    def version(self) -> int:
+        """[summary]
+
+        Returns:
+            int: [Returns the version of the API(2 or 3)]
+        """
+        async with ClientSession() as session:
+            async with session.get(self.url+"version") as data:
+                content = json.loads(data.text())["version"]
+                return content
+
+    @property
+    def type(self) -> str:
+        """[summary]
+
+        Returns:
+            str: [Return the backend used by the API(either PHP or FTL)]
+        """
+        async with ClientSession() as session:
+            async with session.get(self.url+"type") as data:
+                content = json.loads(data.text())["type"]
+                return content
+
+    @property
+    def summaryRaw(self) -> dict:
+        """[summary]
+
+        Returns:
+            dict: [Gives statistics in raw format(no number formatting applied)]
+        """
+        async with ClientSession() as session:
+            async with session.get(self.url+"summaryRaw") as data:
+                content = json.loads(data.text())
+                return content
+
+    @property
+    def summary(self) -> dict:
+        """[summary]
+
+        Returns:
+            dict: [Gives statistics in formatted style]
+        """
+        async with ClientSession() as session:
+            async with session.get(self.url+"summary") as data:
+                content = json.loads(data.text())
+                return content
+
+    @property
+    def overTimeData10mins(self) -> dict:
+        """[summary]
+
+        Returns:
+            dict: [Data needed for generating the domains/ads over time graph on the Pi-hole web dashboard]
+        """
+        async with ClientSession() as session:
+            async with session.get(self.url+"overTimeData10mins") as data:
+                content = json.loads(data.text())
+                return content
+
+    @property
+    def topItems(self, item_amount: int) -> dict:
+        """[summary]
+
+        Args:
+            item_amount (int): [Data needed for generating the Top Client list and Top Advertisers Lists]
+
+        Returns:
+            dict: [last n amount of queries]
+        """
+        async with ClientSession() as session:
+            async with session.get(self.url+f"topItems={item_amount}") as data:
+                content = json.loads(data.text())
+                return content       
+
+    @property
+    def getQueryTypes(self) -> dict:
+        """[summary]
+
+        Returns:
+            dict: [Shows number of queries that the Pi-hole's DNS server has processed]
+        """
+        async with ClientSession() as session:
+            async with session.get(self.url+"getQueryTypes") as data:
+                content = json.loads(data.text())
+                return content
+
+    @property
+    def getAllQueries(self) -> dict:
+        """[summary]
+
+        Returns:
+            dict: [First column: Timestring of query,
+            Second column: Type of query(IPv4 or IPv6),
+            Third column: Requested domain name,
+            Fourth column: Requesting client,
+            Fifth column: Answer type(1 = blocked by gravity.list, 2 = forwarded to upstream server, 3 = answered by local cache, 4 = blocked by wildcard blocking)]
+        """
+        async with ClientSession() as session:
+            async with session.get(self.url+"getAllQueries") as data:
+                content = json.loads(data.text())
+                return content
+
+    @property
+    def recentBlocked(self) -> dict:
+        async with ClientSession() as session:
+            async with session.get(self.url+"recentBlocked") as data:
+                content = json.loads(data.text())
+                return content
+        
+
+
+
