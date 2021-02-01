c08411bc13afc6cabe0b08402686b357ee7a2bd9
CuzImClicks <bruno778.whiteelfie@gmail.com>
2021-02-01 14:50:51 +0100

    added nbt reader

---
+++ clicks_util/nbt_data.py
@@ -0,0 +1,14 @@
+import base64
+import io
+from nbt import nbt
+
+def decode_inventory_data(raw, write_file=False):
+    data = nbt.NBTFile(fileobj=io.BytesIO(base64.b64decode(raw)))
+    if write_file:
+        data.write_file("test.dat")
+    else:
+        return data
+
+
+
+
