f5f2eb459a68c693ed78a09676b82816707e1f5f
CuzImClicks <bruno778.whiteelfie@gmail.com>
2021-02-16 12:48:43 +0100

    added saving the infos to json

--- clicks_util/info.py
+++ clicks_util/info.py
@@ -1,5 +1,5 @@
 import inspect
-
+import json
 
 def getAllFuncs(cls) -> list:
     """Teturn all callable functions of a class"""
@@ -10,11 +10,20 @@ def getAllVars(cls) -> list:
     """Return all variables of a class"""
     return [var for var in vars(cls) if not str(var).startswith("__")]
 
+def getAllDVars(cls) -> list:
+    """Return all variables of a class"""
+    return [var for var in vars(cls) if str(var).startswith("__")]
 
-def getInfo(target) -> dict:
+def save(content: dict):
+    with open("save.json", "w+") as f:
+        json.dump(content, f, indent=2)
+
+
+def getInfo(target, _save: bool = False) -> dict:
+    return_dict = {}
     if not inspect.isclass(target):
         if inspect.isfunction(target):
-            return {
+            return_dict = {
                 "name": target.__name__,
                 "type": type(target),
                 "arg_count": target.__code__.co_argcount,
@@ -27,7 +36,7 @@ def getInfo(target) -> dict:
             }
 
         elif inspect.ismethod(target) or inspect.isbuiltin(target):
-            return {
+            return_dict =  {
                 "name": target.__name__,
                 "qualname": target.__qualname__,
                 "type": type(target),
@@ -42,18 +51,25 @@ def getInfo(target) -> dict:
             "name": target.__name__,
             "module": target.__module__,
             "qualname": target.__qualname__,
-            "doc": target.__doc__,
             "functions": getAllFuncs(target),
+            "magic_methods": getAllDVars(target)
         }
+        if "__slots__" in getAllFuncs(target):
+            return_dict["slots"] = target.__slots__
 
         if inspect.isbuiltin(target):
             return_dict["file"] = inspect.getfile(target)
-            return return_dict
-        return return_dict
+            
 
-    return {
-            "value": target,
-            "type": str(type(target)).replace("<", "").replace(">", "").replace("'", "").split(" ")[1],
-        }
+    if len(return_dict.keys()) == 0:
+        
 
-print(getInfo(int))
+        return_dict = {
+                "value": target,
+                "type": str(type(target)).replace("<", "").replace(">", "").replace("'", "").split(" ")[1],
+                "length": len(target)
+            }
+    if _save:
+        save(return_dict)
+        
+    return return_dict
