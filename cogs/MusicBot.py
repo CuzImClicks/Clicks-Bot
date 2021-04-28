6a34ff6def3e508fd1fbe7b6c19f489f1e1920fe
CuzImClicks <bruno778.whiteelfie@gmail.com>
2021-04-28 10:47:20 +0200

    added return

--- cogs/MusicBot.py
+++ cogs/MusicBot.py
@@ -296,6 +296,7 @@ class MusicBot(commands.Cog):
             else:
                 infoEmbed = discord.Embed(description="Searching songs is currently disabled due to an error with the Genius API", colour=config.getDiscordColour("red"))
                 await ctx.send(embed=infoEmbed)
+                return
                 convertTuple = lambda tup: str(tup).replace("(", "").replace("'", "").replace(")", "").replace(",", "")
                 msg = convertTuple(args)
                 infoEmbed = discord.Embed(title=f"Searching song '{msg}'")
